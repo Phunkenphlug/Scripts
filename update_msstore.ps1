8bf74a2d2b005b994ba56d4b4fe6b46633d6aaeb
Phunkenphlug <74831469+Phunkenphlug@users.noreply.github.com>
2024-12-10 16:31:20 +0100

    Create update_msstore.ps1

---
+++ update_msstore.ps1
@@ -0,0 +1,17 @@
+# Remember old value if store is blocked i.e. by gpo
+$oldValue = (get-itemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windowsstore -Name requirePrivateStoreOnly).requirePrivateStoreOnly
+
+# enable store if prohibited
+if ($oldValue -in (0,1)) { set-itemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windowsstore -Name RequirePrivateStoreOnly -Value 0 -Type DWord }
+
+# open store on downloadpage
+start ms-windows-store://downloadsandupdates
+
+# reset Store and download again
+wsreset.exe -i
+
+# trigger updates of the store apps
+Get-CimInstance -Namespace "Root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod -MethodName UpdateScanMethod
+
+# restore old value for the policy
+if ($oldValue-in (0,1)) { set-itemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windowsstore -Name RequirePrivateStoreOnly -Value $oldValue -Type DWord }
